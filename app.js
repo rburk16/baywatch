609f5c8917b31830440a23324064ca831f0581df
Ryan Burkholder <rcburkholder@anderson.edu>
2017-06-28 17:13:16 -0400

    Commits from class

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 const app = {
-    init: function(formSelector){
-        document.querySelector(formSelector)
-        .addEventListener("submit", this.handleSubmit)
+    init: function(selectors){
+        this.flicks = [//add flick id and all to array]
+        this.max = 0
+        this.list = document.querySelector(selectors.listSelector)
+
+        document.querySelector(selectors.listSelector)
+        .addEventListener('submit', this.handleSubmit.bind(this))
     },
+
+    renderListItem: function(flick) {
+        const item = document.createElement('li')
+        item.textContent = flick.name
+        return item
+    },
+
 
     handleSubmit: function(ev){
         ev.prevenDefault()
         const f= ev.target
-        console.log(f.flickName.value)
+        const flick = {
+            id: this.max + 1,
+            name: f.flickName.value,
+        }
+
+        const listItem = this.renderListItem(flick)
+        this.list.appendChild(listItem)
+
+        this.max ++
+
     },
 }
 
-
-
-app.init()
+app.init({
+    formSelector: 'form#flick-form',
+    listSelector: '#flick-list',
+}
+)
